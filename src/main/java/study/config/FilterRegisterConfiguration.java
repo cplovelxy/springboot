b4ea2bcd59ea105caf89aec34229742b1dcadc5b
cp <1760355704@qq.com>
2017-12-10 14:44:36 +0800

    2017/12/10 解决项目依赖问题

--- src/main/java/study/config/FilterRegisterConfiguration.java
+++ src/main/java/study/config/FilterRegisterConfiguration.java
@@ -1,6 +1,6 @@
 package study.config;
 
-import com.feidee.money.adminauth.filter.AdminAuthFilter;
+
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
